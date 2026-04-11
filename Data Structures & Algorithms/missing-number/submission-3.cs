25714a71a667bb517c0d7e48b9a0d6cc2bb78550
Mitchell Footer <100070700+mfoot123@users.noreply.github.com>
2026-04-10 22:41:36 -0700

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int xor = nums.Length;
+
+        for (int i = 0; i < nums.Length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+}
